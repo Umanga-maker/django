5fa156d2678db5566e37d6f571c6ff4ba2b919cc
Umanga-maker <limbuu063@gmail.com>
2025-05-14 17:33:15 +0545

    todays class

---
+++ python assignment/# Multiple lines.py
@@ -0,0 +1,20 @@
+# Multiple lines
+multiline = '''
+Hey , how are you?
+
+I was just checking in.
+                                All good?
+
+'''
+print (multiline)
+
+# Escaping special characters
+sentence = 'I\'m back at work!\tHey!\n\nWhere\'s this at\\located?'
+print(sentence)
+
+# String Methods
+
+print(first)
+print(first.lower())
+print(first.upper())
+print(first)
