cb099c119e252c6aca956b639c0f3c02cd3e7520
Umanga-maker <limbuu063@gmail.com>
2025-05-20 12:12:06 +0545

    Add files via upload

---
+++ Day 13/elif.py
@@ -0,0 +1,10 @@
+academic_education = False
+IT_experience = True
+if academic_education == True and IT_experience == True:
+    print("you can come and join skill shikshya")
+elif academic_education == True and IT_experience == False:
+    print("Welcome to skill shikshya. No need to worry")
+elif academic_education == False and IT_experience == True:
+    print("Come and join skill shikshya & enhance your skills")
+else:
+    print("Sorry...seatings are packed")
