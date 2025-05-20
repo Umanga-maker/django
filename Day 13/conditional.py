3651806eb84654a055490c82e9b710674d6fe0b3
Umanga-maker <limbuu063@gmail.com>
2025-05-20 12:11:52 +0545

    Add files via upload

---
+++ Day 13/conditional.py
@@ -0,0 +1,11 @@
+weather = str("it is Raining")
+if "it is raining":
+    print("bring an umbrella")
+else:
+    print("no need")
+
+age = int(input("Enter your age: "))
+if age > 16:
+    print("eligible to go to college")
+else:
+    print("not eligible for college")
